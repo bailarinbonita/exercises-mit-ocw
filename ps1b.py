f619733c9f48e91c4deb6c016bb10c45971af7bb
bailarinbonita <langetamar@gmail.com>
2019-07-27 12:46:35 -0700

    adds comments and removes for loop

--- ps1b.py
+++ ps1b.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 26 14:37:29 2019
+Created on Sat Jul 27 12:32:24 2019
 
 @author: tamar
 """
@@ -11,21 +11,27 @@ portion_saved = float(input('Enter the percent of your salary to save, as a deci
 total_cost = float(input('Enter the cost of your dream home: '))
 semi_annual_raise = float(input('Enter the semi-annual raise, as a decimal: '))
 
+#user-provided variables
+
 portion_down_payment = total_cost * 0.25
 current_savings = 0
 r = 0.04
 months = 0
 
+#portion down payment and r are fixed variables. r is the return rate on investments.
+#portion down payment is a quarter of the total cost of the house. the algorithm
+#ceases when current savings exceeds the portion down payment.
+
 while portion_down_payment > current_savings:
-
-    for i in range(1):
-        months += 1
-
-        if months % 6 == 0:
-            annual_salary += annual_salary * semi_annual_raise
-        else:
-            pass
-
+    months += 1
+    if months % 6 == 0:
+        annual_salary += annual_salary * semi_annual_raise
+    else:
+        pass
     current_savings += (portion_saved * (annual_salary / 12)) + ((current_savings * r) / 12)
+
+#basically, the only changes between ps1a and ps1b are the additions of another user-
+#defined variable and the conditional in the while loop to account for the change in
+#salary over time.
 
 print('Number of months:', months)
